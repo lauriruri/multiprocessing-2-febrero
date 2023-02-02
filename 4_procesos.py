ccd6b9a330e9b92cc6a87a73d4bc75d56279ce48
paablomateos <pab02@ucm.es>
2023-02-02 10:39:53 +0100

    actualizado el 4

--- 4_procesos.py
+++ 4_procesos.py
@@ -4,7 +4,7 @@ from multiprocessing import Value
 
 def f(c):
 	for i in range(100):
-	c.value = c.value + 1
+	    c.value = c.value + 1
 	print (f"hola soy {current_process().pid}, vuelta: {i}, contador: {c.value}")
 	
 def g():
@@ -13,7 +13,7 @@ def g():
 if __name__ == "__main__":
 	N = 8
 	lp = []
-	c = Value(’i’, 0)
+	c = Value('i', 0)
 	for i in range(N):
 		lp.append(Process(target=f, args=(c,)))
 	print (f"Valor inicial del contador {c.value}")
